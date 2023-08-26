ece27e3d2741c06a3e3360537cfb9f1f0f26b23e
Priyank.Pathak <priyank.pathak@servicemax.com>
2023-08-26 21:35:56 +0530

    [some types]

---
+++ types-and-saftey/src/main.rs
@@ -0,0 +1,48 @@
+fn main() {
+    println!("Hello, world!");
+
+    let x: (char, u32, i32) = ('n', 234, -1);
+    println!("the first tuple type {:?}", x);
+    let emptyTuple: () = ();
+    /*
+    The {:?} format specifier is for debugging,
+    and it works if all the types inside
+    the tuple implement the Debug trait
+    */
+    println!("printing empty tuple {:?}", emptyTuple);
+
+    #[derive(Debug)]
+    struct Custom {
+        x: f32,
+        y: f32,
+    };
+
+    let instance_of_custom_struct: Custom = Custom { x: 34.4, y: 45.4 };
+    println!("print a custom struct {:?}", instance_of_custom_struct);
+
+    #[derive(Debug)]
+    struct T(i32, char);
+
+    let tuple_type: T = T(234, 'e');
+    println!("printing the tuple type {:?}", tuple_type);
+
+    #[derive(Debug)]
+    struct E;
+
+    let m: E = E;
+    println!("{:?}", m);
+
+    #[derive(Debug)]
+    enum Attend {
+        OnTime,
+        Late(u32),
+    }
+
+    let enumm: Attend = Attend::OnTime;
+    let enumm2: Attend = Attend::Late(34);
+    println!("enum printing {:?}, {:?}", enumm, enumm2);
+
+    let x: Attend = Attend::Late(15);
+    let boxx = Box::new(x);
+    println!("printing Box.Attend {:?}", boxx);
+}
